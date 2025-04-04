6c7b7e0a00fb00a57f62052059f4fe19a65b0a18
patelpk3 <patelpk3@miamioh.edu>
2025-04-03 20:57:39 -0400

    setting up class

--- clue.java
+++ clue.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+import java.awt.*;
+
 package fullHouse;
 
 public class clue {
+    private boolean hasPuzzle;
+    private boolean puzzleComplete;
+    private String imagePath;  // Path to clue image
+    private String description; // Clue text description
+    
     
 }
