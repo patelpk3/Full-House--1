f645df8b0f6f91a0bfe793bb9286b5f32e5eb77f
patelpk3 <patelpk3@miamioh.edu>
2025-04-06 19:41:41 -0400

    making sure the code is up to date and follows the coding standard rubric

--- basicClue.java
+++ basicClue.java
@@ -1,22 +1,41 @@
 package fullHouse;
 
-public class basicClue {
-    private boolean found;
+/**
+ * Class: BasicClue
+ * @author Parth, Kyle, Josh, Kengo
+ * @version 1.0
+ * Course: CSE 201 Spring 2025
+ * Written: April 6, 2025
+ *
+ * Purpose: Shows a basic clue that tracks whether it has been found or not.
+ */
+public class BasicClue {
+    private boolean found; // Shows if the clue has been found
 
-    // constructor
-    public basicClue(boolean found) {
+    /**
+     * Constructor to show the BasicClue object.
+     *
+     * @param found boolean showing if the clue has been found
+     */
+    public BasicClue(boolean found) {
         this.found = found;
     }
 
+    /**
+     * Determines if the clue has been found.
+     *
+     * @param found the current found status
+     * @return true if the clue has been found, false if not
+     */
     public boolean clueFound(boolean found) {
         boolean clueFound = false;
 
-        if(found) {
+        if (found) {
             clueFound = true;
         } else {
             clueFound = false;
         }
 
         return clueFound;
-    } 
+    }
 }
