aa7e720406ba441b781774dc6e76697acf72f2f6
thebeard7 <beardkn@miamioh.edu>
2025-04-06 17:22:08 -0400

    Update puzzle.java

--- puzzle.java
+++ puzzle.java
@@ -1,5 +1,18 @@
 package fullHouse;
 
 public class puzzle {
-    
+    private boolean puzzleComplete;
+
+    boolean setPuzzleComplete(boolean puzzleComplete) {
+        this.puzzleComplete = puzzleComplete;
+        return puzzleComplete;
+    }
+
+    boolean getPuzzleComplete(boolean puzzleComplete) {
+        return puzzleComplete;
+    }
+
+    void puzzle(boolean puzzleComplete) {
+        System.out.println("This is the puzzle method");
+    }
 }
