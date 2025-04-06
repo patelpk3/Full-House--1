e9c37c7702c0287d12f39537b67a6bf04f2672cd
patelpk3 <patelpk3@miamioh.edu>
2025-04-06 19:34:37 -0400

    making sure the code is up to date and follows the coding standard rubric

--- fullHouse.java
+++ fullHouse.java
@@ -1,7 +1,23 @@
 package fullHouse;
 
-public class fullHouse {
+/**
+ * Class: FullHouse
+ * @author Parth, Kyle, Josh, Kengo
+ * @version 1.0
+ * Course: CSE 201 Spring 2025
+ * Written: April 6, 2025
+ *
+ * Purpose: This is the main class for our Full House game.
+ * It is where the program starts.
+ */
+public class FullHouse {
+
+    /**
+     * Main method to start the Full House game.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
-        
+
     }
 }
