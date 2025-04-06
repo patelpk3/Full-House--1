03fed7684e3f7c9cacead5da5f8e165de9480fe9
patelpk3 <patelpk3@miamioh.edu>
2025-04-06 19:23:40 -0400

    making sure the code is up to date and follows the coding standard rubric

--- puzzle.java
+++ puzzle.java
@@ -1,18 +1,44 @@
 package fullHouse;
 
-public class puzzle {
-    private boolean puzzleComplete;
+/**
+ * Class: Puzzle
+ * @author Parth, Kyle, Josh, Kengo
+ * @version 1.0
+ * Course: CSE 201 Spring 2025
+ * Written: April 6, 2025
+ *
+ * Purpose: A puzzle object within a room in our Full House game.
+ * Keeping track if the puzzle has been completed.
+ */
+public class Puzzle {
+    private boolean puzzleComplete; // Shows if the puzzle is complete
 
-    boolean setPuzzleComplete(boolean puzzleComplete) {
+    /**
+     * Sets the status of the puzzle.
+     *
+     * @param puzzleComplete boolean showing whether the puzzle is complete
+     * @return boolean status that was set
+     */
+    public boolean setPuzzleComplete(boolean puzzleComplete) {
         this.puzzleComplete = puzzleComplete;
         return puzzleComplete;
     }
 
-    boolean getPuzzleComplete(boolean puzzleComplete) {
-        return puzzleComplete;
+    /**
+     * Gets the completion status of the puzzle.
+     *
+     * @return boolean showing the puzzle's current completion status
+     */
+    public boolean getPuzzleComplete() {
+        return this.puzzleComplete;
     }
 
-    void puzzle(boolean puzzleComplete) {
+    /**
+     * Method representing a puzzle interaction.
+     *
+     * @param puzzleComplete 
+     */
+    public void puzzle(boolean puzzleComplete) {
         System.out.println("This is the puzzle method");
     }
 }
