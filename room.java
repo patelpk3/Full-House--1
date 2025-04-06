3f653e3e1b1317efabd7cf42be548db4f8c7ec82
patelpk3 <patelpk3@miamioh.edu>
2025-04-06 19:15:04 -0400

    making sure the code is up to date and follows the coding standard rubric

--- room.java
+++ room.java
@@ -1,37 +1,71 @@
 package fullHouse;
 
-public class room {
-    private clue roomClue;
-    private String location;
-    private String roomDescription;
-    private boolean roomComplete;
+/**
+ * Class: Room
+ * @author Parth, Kyle, Josh, Kengo
+ * @version 1.0
+ * Course: CSE 201 Spring 2025
+ * Written: April 6, 2025
+ *
+ * Purpose: Represents a room in the Full House game.
+ * Each room can contain a clue, have a description, and
+ * track whether it has been completed.
+ */
+public class Room {
+    private Clue roomClue; // Clue object in the room
+    private String location; // Name of the room
+    private String roomDescription; // Description of the room's environment
+    private boolean roomComplete; // Indicates if the room has been completed
 
-    // constructor
-    public room(clue roomClue, String location, String roomDescription) {
+    /**
+     * Constructor to initialize a Room object.
+     *
+     * @param roomClue The clue in the room
+     * @param location The room's location identifier
+     * @param roomDescription A brief description of the room
+     */
+    public Room(Clue roomClue, String location, String roomDescription) {
         this.roomClue = roomClue;
         this.location = location;
         this.roomDescription = roomDescription;
         this.roomComplete = false;
     }
 
+    /**
+     * Method to create the starting room.
+     */
     public void startingRoom() {
-        //clue start = new clue(roomComplete, location, roomDescription);
-        room startingRoom = new room(roomClue, "This is the starting room.", 
-        "You may find clues here but you can manuver around to find out.");
+        Room startingRoom = new Room(roomClue, 
+            "This is the starting room.", 
+            "You may find clues here but you can maneuver around to find out.");
     }
 
+    /**
+     * Method to describe the room.
+     */
     public void describeRoom() {
         System.out.println("This is the describeRoom method.");
     }
 
+    /**
+     * Method to give a clue in the room.
+     */
     public void giveClue() {
         System.out.println("This is the giveClue method.");
     }
 
+    /**
+     * Method to replicate talking in the room.
+     */
     public void dialogue() {
         System.out.println("This is the dialogue method.");
     }
 
+    /**
+     * Method to replicate investigating the room.
+     *
+     * @return boolean showing whether something was found
+     */
     public boolean investigate() {
         return false;
     }
