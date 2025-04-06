bae76a674dd3627688692bf7e6ae23ab39f84f29
thebeard7 <beardkn@miamioh.edu>
2025-04-06 18:07:11 -0400

    Update player.java

--- player.java
+++ player.java
@@ -1,5 +1,38 @@
 package fullHouse;
 
 public class player {
-    
+    private int moves;
+    private int cluesFound;
+
+    public void setCluesFound(int cluesFound) {
+        this.cluesFound = cluesFound;
+    }
+
+    public int getCluesFound() {
+        return cluesFound;
+    }
+
+    public void setMoves(int moves) {
+        this.moves = moves;
+    }
+
+    public int getMoves() {
+        return moves;
+    }
+
+    public void intro() {
+        System.out.println("This is the intro method.");
+    }
+
+    public void help() {
+        System.out.println("This is the help method.");
+    }
+
+    public boolean win() {
+        return false;
+    }
+
+    public boolean lose() {
+        return false;
+    }
 }
