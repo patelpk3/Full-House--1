7afb8cf1e363ea3910b834d7a41cd1ac05e13f56
patelpk3 <patelpk3@miamioh.edu>
2025-04-06 19:39:04 -0400

    making sure the code is up to date and follows the coding standard rubric

--- clue.java
+++ clue.java
@@ -1,15 +1,31 @@
+package fullHouse;
+
 import javax.swing.*;
 import java.awt.*;
 
-package fullHouse;
+/**
+ * Class: Clue
+ * @author Parth, Kyle, Josh, Kengo
+ * @version 1.0
+ * Course: CSE 201 Spring 2025
+ * Written: April 6, 2025
+ *
+ * Purpose: All the clues in the Full House game.
+ * A clue can be a puzzle, an image, and/or a textual description.
+ */
+public class Clue {
+    private boolean hasPuzzle; // Shows if the clue has a puzzle
+    private boolean puzzleComplete; // Tracks if the puzzle has been completed
+    private String imagePath; // Path to the clue image
+    private String description; // Text description of the clue
 
-public class clue {
-    private boolean hasPuzzle;
-    private boolean puzzleComplete;
-    private String imagePath;  // Path to clue image
-    private String description; // Clue text description
-
-    // Constructor
+    /**
+     * Constructor to show a Clue object.
+     *
+     * @param hasPuzzle whether the clue has a puzzle
+     * @param imagePath file path to the clue image
+     * @param description text description of the clue
+     */
     public Clue(boolean hasPuzzle, String imagePath, String description) {
         this.hasPuzzle = hasPuzzle;
         this.puzzleComplete = false;
@@ -17,12 +33,18 @@ public class clue {
         this.description = description;
     }
 
-    // Getter for puzzleComplete
+    /**
+     * Returns whether the puzzle with the clue is complete.
+     *
+     * @return true if the puzzle is complete, false if otherwise
+     */
     public boolean isPuzzleComplete() {
         return puzzleComplete;
     }
 
-    // Set puzzle as complete
+    /**
+     * Marks the puzzle as complete if it exists and displays the clue image in the room.
+     */
     public void completePuzzle() {
         if (hasPuzzle) {
             this.puzzleComplete = true;
@@ -32,13 +54,17 @@ public class clue {
             System.out.println("This clue doesn't have a puzzle.");
         }
     }
-    
-    // Shows clue description
+
+    /**
+     * Shows the description of the clue.
+     */
     public void clueDescription() {
         System.out.println("Clue Description: " + description);
     }
-    
-    // Show image in popup
+
+    /**
+     * Shows the clue image in the game.
+     */
     private void showClueImage() {
         JFrame frame = new JFrame("Clue Found!");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -52,16 +78,21 @@ public class clue {
         frame.setVisible(true);
     }
 
-    // For testing
+    /**
+     * Main method for testing the Clue class.
+     *
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
-        Clue kingSuiteClue = new Clue(true, "images/kingsuite_clue.png",
-                "A folded piece of paper under the nightstand. It reads: 'The last card lies in velvet.'");
+        Clue kingSuiteClue = new Clue(
+            true,
+            "images/kingsuite_clue.png",
+            "A folded piece of paper under the nightstand. It reads: 'The last card lies in velvet.'"
+        );
 
         kingSuiteClue.clueDescription();
 
-        // Simulate solving the puzzle
+        // Simulates solving the puzzle
         kingSuiteClue.completePuzzle();
     }
-    
-    
 }
